e1c3413ebc2d211029699b49cdd1739bf9726abe
Griezmann2003 <garciagomezgamaliel@gmail.com>
2025-08-25 13:29:01 -0600

    Actualizar color primario en AppPanelProvider a rojo (#FF0000).

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -29,7 +29,7 @@ class AppPanelProvider extends PanelProvider
             ->path('app')
             ->login()
             ->colors([
-                'primary' => '#e40b2fff',
+                'primary' => '#FF0000',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
