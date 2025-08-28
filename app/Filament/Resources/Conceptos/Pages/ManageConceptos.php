72882de38330a9bfeec35cb2a0ff1fab1d31b212
Griezmann2003 <garciagomezgamaliel@gmail.com>
2025-08-27 20:53:45 -0600

    se oculta el boton de crear y crear otro

--- app/Filament/Resources/Conceptos/Pages/ManageConceptos.php
+++ app/Filament/Resources/Conceptos/Pages/ManageConceptos.php
@@ -13,7 +13,7 @@ class ManageConceptos extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->createAnother(false),
         ];
     }
 }
